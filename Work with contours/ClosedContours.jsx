d68b745e3206880c1a082e48b9ff22413881c390
DanteOnline <iamdanteonline@gmail.com>
2016-08-23 14:38:23 +0300

    Init scripts

---
+++ Work with contours/ClosedContours.jsx
@@ -0,0 +1,7 @@
+﻿if ( app.documents.length > 0 && app.activeDocument.pathItems.length > 0 ) {
+doc = app.activeDocument;
+for ( i = 0; i < doc.pathItems.length; i++ ) {
+pathRef = doc.pathItems[i];
+pathRef.closed = true;
+}
+}
